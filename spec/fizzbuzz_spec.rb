656696fb1e3bddafc2eaac14c8094c2bd98e92d8
Erik Björn <erik.g.bjoern@gmail.com>
2020-03-23 10:12:37 +0100

    creating test for 1 and 3 returning 1 and 'fizz'

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,8 +2,12 @@ require "./lib/fizzbuzz.rb"
 
 describe "fizzbuzz" do
         
-    it "returns 1 if number is 1" do
+    it 'returns 1 if number is 1' do
             expect(fizzbuzz(1)).to eq 1
+    end
+
+    it 'returns "fizz" if number is divisible by 3' do
+        expect(fizzbuzz(3)).to eq "fizz"
     end
 
 end
